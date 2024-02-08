8be22d0e7c0d23d5d214ea5783bf345c0cc448ea
Ensify <vignesh1234can@gmail.com>
2024-02-08 11:40:51 +0530

    minor maiyru changes

--- speak.py
+++ speak.py
@@ -6,7 +6,7 @@ class Pesuda:
     def __init__(self):
         device = f'cuda:{torch.cuda.current_device()}' if torch.cuda.is_available() else 'cpu'
         self.model = Text2Speech.from_pretrained("espnet/kan-bayashi_ljspeech_vits",device = device)
-        fs = self.model.fs
+        self.fs = self.model.fs
     
     def pesu(self,text):
         speech = self.model(text)["wav"]
