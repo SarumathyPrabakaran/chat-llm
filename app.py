4cb7f7e46a772067e9b013175d82933ba46b75e6
Ensify <vignesh1234can@gmail.com>
2024-02-08 14:45:01 +0530

    speech fixes

--- app.py
+++ app.py
@@ -18,16 +18,18 @@ def get_answer(audio_file: UploadFile = File(...), name: str = Form(...)):
     except:
         return 0
     
-    print(f"Took {time.time() - t} seconds for speech recognition.")
+    print(f"Took {time.time() - t} seconds for speech recognition. {question}" )
     answer = chat.ask(question, name)
-    print(f"Took {time.time() - t} seconds till generating output.")
+    answer = answer.replace("AWS","A. W S")
+    answer = answer.replace("AI","A. I")
+    print(f"Took {time.time() - t} seconds till generating output. {answer}")
     audio = speaker.render(answer)
     print(f"Took {time.time() - t} seconds till pesurathu")
 
     return FileResponse(audio)
 
 if __name__ == '__main__':
-    chat = ChatBot("context.txt")
     speech = SpeechText()
     speaker = TextSpeech()
+    chat = ChatBot("context.txt")
     uvicorn.run(app, host="0.0.0.0", port=8000)
