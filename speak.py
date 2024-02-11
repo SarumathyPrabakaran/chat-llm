02079d2fdbba488e0ad2b176e02111d06468fe99
Ensify <vignesh1234can@gmail.com>
2024-02-11 15:24:05 +0530

    docs

--- speak.py
+++ speak.py
@@ -5,10 +5,22 @@ import soundfile
 class TextSpeech:
 
     def __init__(self):
+        """
+        Initialize the Text2Speech model.
+        """
         device = f'cuda:{torch.cuda.current_device()}' if torch.cuda.is_available() else 'cpu'
         self.speech_model = Text2Speech.from_pretrained("espnet/kan-bayashi_ljspeech_vits",device = device)
     
-    def render(self, text):
-        speech = self.speech_model(text)["wav"]
+    def render(self, text: str) -> str:
+        """
+        Render the text to speech and save it as a wav file.
+
+        Args:
+        text (str): The input text to be synthesized.
+
+        Returns:
+        str: The path of the generated wav file.
+        """
+        speech = self.speech_model(text=text)["wav"]
         soundfile.write("out.wav", speech.cpu().numpy(), self.speech_model.fs, "PCM_16")
         return "out.wav"
